81dac380081d3fa88c805b210ccec7a17fef4df5
Shreejanshres <shreejan.shrestha2003@gmail.com>
2025-04-14 22:45:57 +0545

    fix

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 require("dotenv").config();
+const allowedOrigins = ["http://localhost:5173", "https://yourdomain.com"];
+
 const corsOptions = {
-    origin: 'http://localhost:5173', // The exact origin you want to allow
-    credentials: true, // Allow credentials (cookies, HTTP authentication)
+	origin: (origin, callback) => {
+		if (allowedOrigins.includes(origin)) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+	credentials: true,
 };
 const authRoutes = require("./routes/auth");
 const authMiddleware = require("./middleware/authmiddleware");
@@ -24,7 +32,7 @@ app.get("/", (req, res) => res.send("API running"));
 app.use("/", authRoutes);
 
 sequelize
-	.sync({ alter: true }) 
+	.sync({ alter: true })
 	.then(() => {
 		app.listen(process.env.PORT, () => {
 			console.log(`Server running on port ${process.env.PORT}`);
